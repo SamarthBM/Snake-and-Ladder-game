bf9e7d329e10936827a506c780015d3fdf1349fb
SamarthBM <samarthbm18@gmail.com>
2021-07-02 18:39:45 +0530

    Added random function to generate dice number

--- src/com/bridgelabs/SnakeAndLadder.java
+++ src/com/bridgelabs/SnakeAndLadder.java
@@ -6,6 +6,8 @@
 
 package com.bridgelabs;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	// Initializing first player position to 0
@@ -13,7 +15,11 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
-		System.out.println("Your position is " + player1);
+		
+		Random random = new Random();
+		
+		int dice_num = 1+random.nextInt(6);
+		System.out.println("Your dice number is: " + dice_num);
 
 	}
 
