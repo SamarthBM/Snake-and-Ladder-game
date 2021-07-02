9e3d702f9316efcb4a3ab08b8b1ad8ed4d87eb75
SamarthBM <samarthbm18@gmail.com>
2021-07-02 18:04:33 +0530

    Initial commit

---
+++ src/com/bridgelabs/SnakeAndLadder.java
@@ -0,0 +1,18 @@
+/****************************************************************
+ * Purpose : To simulate a snake and ladder game
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.bridgelabs;
+
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to snake and ladder game");
+
+	}
+
+}
+
