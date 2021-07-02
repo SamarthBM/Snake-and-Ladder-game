3dcbf6eb8d06615197784f69803550eb2119f295
SamarthBM <samarthbm18@gmail.com>
2021-07-02 19:11:54 +0530

    Generated options for the player: No play, ladder or snake

--- src/com/bridgelabs/SnakeAndLadder.java
+++ src/com/bridgelabs/SnakeAndLadder.java
@@ -10,16 +10,44 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
-	// Initializing first player position to 0
-	public static final int player1 = 0;
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
-		
+
+		// Initializing first player position to 0
+		int player1 = 0;
+
 		Random random = new Random();
-		
-		int dice_num = 1+random.nextInt(6);
+
+		int dice_num = 1 + random.nextInt(6);
 		System.out.println("Your dice number is: " + dice_num);
+		
+		// Generated random numbers to get the options.
+		int option = random.nextInt(3);
+
+		switch (option) {
+
+		case 0:
+			System.out.println("There are no play");
+			break;
+
+		case 1:
+			System.out.println("Ladder");
+			player1 = player1 + dice_num;
+			System.out.println("Your position is: " + player1);
+			break;
+
+		default:
+			System.out.println("Snake");
+			player1 = player1 - dice_num;
+			
+			// Player position will be set to 0, if position is less than 0.
+			if (player1 < 0) {
+				player1 = 0;
+			}
+			System.out.println("Your position is: " + player1);
+			break;
+
+		}
 
 	}
 
